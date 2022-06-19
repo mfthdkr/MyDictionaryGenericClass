d6a7ed148afaf0eb895893bc71af1c586b20ad15
MUHAMMET FATİH DİKER <MUHAMMET FATH DKER@DESKTOP-P96S07M>
2022-06-20 01:05:43 +0300

    Add project files.

---
+++ MyDictionaryGenericClass/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryGenericClass
+{
+    internal class MyDictionary<T1, T2>
+    {
+        private T1[] _arrayKey;
+        private T2[] _arrayValue;
+        private T1[] _tempKey;
+        private T2[] _tempValue;
+
+
+        public MyDictionary()
+        {
+            _arrayKey = new T1[0];
+            _arrayValue = new T2[0];
+        }
+
+        public void Add(T1 item1, T2 item2)
+        {
+            _tempKey = _arrayKey;
+            _tempValue = _arrayValue;
+            _arrayKey = new T1[_arrayKey.Length + 1];
+            _arrayValue = new T2[_arrayValue.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _arrayKey[i] = _tempKey[i];
+            }
+
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _arrayValue[i] = _tempValue[i];
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = item1;
+            _arrayValue[_arrayValue.Length - 1] = item2;
+
+        }
+
+        public int Count
+        {
+            get { return _arrayKey.Length; }
+        }
+    }
+}
---
+++ MyDictionaryGenericClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryGenericClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            Console.WriteLine(dictionary.Count);
+            dictionary.Add(1, "Fatih");
+            dictionary.Add(2, "Ahmet");
+            Console.WriteLine(dictionary.Count);
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add(1, "Fatih");
+            myDictionary.Add(2, "Ali");
+            myDictionary.Add(3, "Mustafa");
+            myDictionary.Add(4, "İsmail");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
